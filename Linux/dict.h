96e0a2e2f62603b86ef6ca0c09f7f7e00632da25
Sahil verma <42215406+iamsahil1910@users.noreply.github.com>
2018-10-22 03:22:01 +0530

    Add files via upload

---
+++ Linux/dict.h
@@ -0,0 +1,25 @@
+
+#ifndef DICT_H
+#define DICT_H
+
+#include <iostream>
+using namespace std;
+
+typedef struct _word
+{
+    struct _word *ch1[26];
+    bool value;
+    char meaning[50];
+}
+word1;
+
+bool load_dict();
+
+void meaning(char*);
+
+bool exist(char*);
+
+bool unload(void);
+
+#endif
+
